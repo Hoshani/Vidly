1eab4b6ac7587971b7a5fca2052894dcb00856d0
hoshani <hoshani.mohammed@gmail.com>
2019-02-13 18:35:52 +0300

    Updated

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,21 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            // this will make routing on controller file instead of creating it here
+            routes.MapMvcAttributeRoutes();
+
+            // the order of routes matters, you need to range them from the most specefic to the most generic
+            // note this approach will create a fragile code
+            routes.MapRoute(
+                "MoviesByReleaseDate", // Name
+                "movies/released/{year}/{month}", // URL with {variables}
+                new { controller = "Movies", action = "ByReleaseDate" }, // defaults for map route
+                new { year = @"\d{4}", month = @"\d{2}" } // regex to validate parameters
+                );
+
+            // this is the default route, if there is no custom route matching it will go here 
+            // note: make it the last route because it will skip the rest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,7 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        // page with string (with layout)
         public ActionResult Random()
         {
 
@@ -22,6 +23,76 @@ namespace Vidly.Controllers
             Movie movie = new Movie() { Name = name };
 
             return View(movie);
+        }
+
+        // GET: Movies/Edit/1
+        // GET: Movies/Edit?id=1
+        // testing a parameter (follows the conventional route {controller}/{action}/{id})
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        // GET: Movies
+        // GET: Movies/Index
+        // GET: Movies/Index?pageindex=1&sortby=name
+        // testing parameters
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex = {0}, SortBy = {1}", pageIndex, sortBy));
+        }
+
+        // GET: Movies/released/2014/04  << year and month
+        // this has a custom route indicated in app_start>>RouteConfig.cs file
+        public ActionResult ByReleaseDate(string year, string month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        // GET: Movies/released/2014   << year only
+        // this is another way to have a custom route but inside the controller file
+        // note: you should have added 'route.MapMvcAttributeRoutes();' inside app_start>>RouteConfig.cs file
+        // adding constraint of range helps you transfer between older and newer version 
+        // (supposing you switched views for only newly added movies in this case)
+        // for more https://blogs.msdn.microsoft.com/webdev/2013/10/17/attribute-routing-in-asp-net-mvc-5/
+        [Route("movies/released/{year:regex(\\d{4}):range(2000,2020)}")]
+        public ActionResult ByReleaseYear(string year)
+        {
+            return Content("Release Year is " + year);
+        }
+
+        // GET: Movies/HelloWorld
+        // page with string (no layout)
+        public ActionResult HelloWorld()
+        {
+            return Content("Hello World");
+        }
+
+        // GET: Movies/Http404
+        // returns HTTP 404
+        public ActionResult Http404()
+        {
+            return HttpNotFound();
+        }
+
+        // GET: Movies/Empty
+        // returns empty page
+        public ActionResult Empty()
+        {
+            return new EmptyResult();
+        }
+
+        // GET: Movies/Redirect
+        // redirect !!
+        public ActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home", new { page = "1", somethingElse = "someString" });
         }
     }
 }
